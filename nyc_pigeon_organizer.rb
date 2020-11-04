8394ec4789488159d67be2008479c04451aaf4e5
Ryan Kendig <ryan.kendig@gmail.com>
2020-11-04 18:30:12 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,6 +6,12 @@ def nyc_pigeon_organizer(data)
       names.each do |name|
         if !final_array{name}
           final_array[name] = {}
+        end
+        if !final_array[name][key]
+          !final_array[name][key] = []
+        end
+        final_array[name][key].push(inner_key)
+      end
     end 
     final_array
   end
