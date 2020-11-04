858bf605a8625149bd7ca03b988529a0b3676609
Ryan Kendig <ryan.kendig@gmail.com>
2020-11-04 18:31:52 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,7 @@
-require 'pry'
+# require 'pry'
 def nyc_pigeon_organizer(data)
   data.each_with_object({}) do |(key, value), final_array|
-    binding.pry
+    # binding.pry
     value.each do |inner_key, names|
       names.each do |name|
         if !final_array{name}
@@ -15,5 +15,5 @@ def nyc_pigeon_organizer(data)
     end 
     final_array
   end
-  binding.pry
+  # binding.pry
 end
